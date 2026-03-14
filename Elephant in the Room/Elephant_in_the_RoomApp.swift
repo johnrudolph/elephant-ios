d1874b06f0309f567b9f86a0936962b6d31734e2
John Drexler <johnrudolphdrexler@gmail.com>
2026-03-14 10:55:42 -0400

    Initial Commit

---
+++ Elephant in the Room/Elephant_in_the_RoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  Elephant_in_the_RoomApp.swift
+//  Elephant in the Room
+//
+//  Created by John Drexler on 3/14/26.
+//
+
+import SwiftUI
+
+@main
+struct Elephant_in_the_RoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Elephant in the RoomUITests/Elephant_in_the_RoomUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Elephant_in_the_RoomUITestsLaunchTests.swift
+//  Elephant in the RoomUITests
+//
+//  Created by John Drexler on 3/14/26.
+//
+
+import XCTest
+
+final class Elephant_in_the_RoomUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
